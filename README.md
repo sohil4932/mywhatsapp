110b47fc0c4385c67f2cfd1b5f9455662c871f20
Sohil Patel <sohilpatel4932@gmail.com>
2015-04-14 02:59:04 +0530

    Addedd simple flask app

---
+++ README.md
@@ -0,0 +1,1 @@
+MyWhatsapp Readme file
---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+# We'll render HTML templates and access data sent by POST
+# using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done
+# and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/')
+def index():
+    return "Hello World"
+
+# Route that will process the text messages
+@app.route('/message/<int:phone_number>/<text_message>', methods=['POST'])
+def message(phone_number, text_message):
+    # Get the data
+    return "Succefully added " + text_message + " for " + str(phone_number)
+
+# Route that will process the file upload
+@app.route('/image/<int:phone_number>', methods=['POST'])
+def upload(phone_number):
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return "Successful Upload for " + str(phone_number) 
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
